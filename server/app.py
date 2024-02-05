6aef8e8d81654f6c07d6b95f0d924ed2bf1bf15f
beatricenjerimwangi <bbeatricemwangi@gmail.com>
2024-02-05 14:56:19 -0500

    created migration

--- server/app.py
+++ server/app.py
@@ -2,15 +2,14 @@ from flask import Flask,jsonify,request,make_response
 from flask_migrate import Migrate
 from flask_restful import Api,Resource
 
-from models import db,Restaurant,RestaurantPizza,Pizza,Rest
+from models import db, Restaurant, RestaurantPizza,Pizza
 
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
-migrate=Migrate(app.db)
+migrate=Migrate(app,db)
 db.init_app(app)
 api=Api(app)
-
 
 
 
