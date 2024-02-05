ef547bf3fe5479602eef9810d01b60b1ad286f1c
beatricenjerimwangi <bbeatricemwangi@gmail.com>
2024-02-05 16:55:23 -0500

    added more seed data

--- server/seed.py
+++ server/seed.py
@@ -17,13 +17,20 @@ def seed_data():
     #restaurants creating
     rest1=Restaurant(name='Cafe',address='Thika')
     rest2=Restaurant(name='demo',address='Tunnel')
+    rest3=Restaurant(name='moringa',address='Ngong')
+    rest4=Restaurant(name='naz',address='Ronga')
+    rest5=Restaurant(name='sonford',address='cbd')
+
 
     First_pizza=Pizza(name='First',ingredients='peperoni')
     Second_pizza=Pizza(name='Second',ingredients='tikka')
+    Third_pizza=Pizza(name='third',ingredients='bbq')
+    Fourth_pizza=Pizza(name='fourth',ingredients='periperi')
+    Fifth_pizza=Pizza(name='fifth',ingredients='tikka')
 
-    db.session.add_all([rest1,rest2,First_pizza,Second_pizza])
+
+    db.session.add_all([rest1,rest2,rest3,rest4,rest5,First_pizza,Second_pizza,Third_pizza,Fourth_pizza,Fifth_pizza])
     db.session.commit()
 
 if __name__ == '__main__':
     seed_data()
-    
