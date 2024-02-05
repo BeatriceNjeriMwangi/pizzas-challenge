6284124abaea2daa6a004de28234d0eb575e9a12
beatricenjerimwangi <bbeatricemwangi@gmail.com>
2024-02-05 16:49:31 -0500

    added get and delete routes

--- server/app.py
+++ server/app.py
@@ -11,8 +11,53 @@ migrate=Migrate(app,db)
 db.init_app(app)
 api=Api(app)
 
+class Restaurants(Resource):
+    def get(self):
+        response_dict=[{"id":restaurant.id,"name":restaurant.name,"address":restaurant.address}for restaurant in Restaurant.query.all()]
+        return make_response(jsonify(response_dict),200)
+    
+class RestaurantById(Resource):
+    def get(self, id):
+        restaurant=Restaurant.query.filter_by(id=id).first().to_dict()
+        if not restaurant:
+            return make_response(jsonify({"error":"Restaurant not found"}))
+        restaurant_data={
+            "id":restaurant.id,
+            "name":restaurant.name,
+            "address":restaurant.address,
+            "pizzas":[]
+            
+        }
+        for pizza in restaurant.pizzas:
+            response={
+                "id":pizza.id,
+                "name":pizza.name,
+                "ingredients":pizza.ingredients
+            }
+        restaurant_data["pizzas"].append(response) 
 
+        return make_response(jsonify(restaurant),200) 
 
+    def delete(Resource):
+        restaurant=Restaurant.query.filter_by(id=id).first().to_dict()
+
+        if not restaurant:
+            return make_response(jsonify({
+  "error": "Restaurant not found"
+}))
+        RestaurantPizza.query.filter_by(id=id).delete()
+        db.session.delete(restaurant)
+        db.session.commit()
+        return make_response(jsonify({}),200)
+    
+class Pizza(Resource):
+    def get(self):
+        pizza=[{"id":pizza.id,"name":pizza.name,"ingredients":pizza.ingredients}for pizza in Pizza.query.all()]
+        return make_response(jsonify(pizza),200)
+    
+api.add_resource(Restaurants,'/restaurants')
+api.add_resource(RestaurantById,'/restaurants/<int:id>')
+api.add_resource(Pizza,'/pizzas')
 
 
 
